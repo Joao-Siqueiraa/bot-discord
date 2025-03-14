6a06aa38ecaf83dda2709e8ad2cef85acc687c73
Siqueira <joaopedrosiqueira2467@gmail.com>
2025-03-14 12:31:36 -0300

    1.0V
    criando meu bot

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands, tasks
+from datetime import time
+from config import TOKEN
+intents = discord.Intents.all()
+bot = commands.Bot(".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot inicializado com sucesso")
+
+
+
+
+bot.run(TOKEN)
