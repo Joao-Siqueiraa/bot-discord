e6b9412fbf5ccb4e8a31edb81e0d75b8f8f94aa7
Siqueira <joaopedrosiqueira2467@gmail.com>
2025-03-14 12:43:46 -0300

    1.2V

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import discord
 from discord.ext import commands, tasks
 from datetime import time
 from config import TOKEN
+
+
+
 intents = discord.Intents.all()
 bot = commands.Bot(".", intents=intents)
 
 @bot.event
 async def on_ready():
     print("Bot inicializado com sucesso")
+
+
 
 
 
